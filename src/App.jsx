894b891e2121973dd38268ae0e7bbd0e05ca63c7
Mulligascos <53591720+Mulligascos@users.noreply.github.com>
2026-02-19 23:29:09 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -514,7 +514,7 @@ const LoginPage = ({ players, onLogin, error, darkMode, setDarkMode, isOnline })
         <div className="text-center mb-12 mt-8">
           <div className="inline-flex items-center justify-center w-20 h-20 rounded-full mb-4 shadow-lg">
             <img
-              src="https://i.imgur.com/JJdyPhS.gif"
+              src="TDGcircle.gif"
               alt="Timaru Disc Golf"
               className="w-36 h-36 object-contain"
             />
