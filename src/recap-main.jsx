f868c59fad298752947c347c4c20b05cde8c3a6a
Mulligascos <53591720+Mulligascos@users.noreply.github.com>
2026-03-02 14:20:53 +1300

    Delete src/recap-main.jsx

--- src/recap-main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SeasonRecap from './SeasonRecap'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SeasonRecap />
-  </React.StrictMode>
-)
